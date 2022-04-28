6c048a0e984494bc5e74a1e5943750b248b191a3
cristian <Cristian_caht@hotmail.com>
2022-04-28 14:59:11 -0500

    Added view

--- api/app/views/UserView.js
+++ api/app/views/UserView.js
@@ -1,5 +1,31 @@
-const UserService = require('./../models/UserService')
+const User = require('./../models/user')
+const UserService = require('./../services/UserService')
 
 class UserView{
-    static create()
+    static createUser(payload){
+        if (payload == null){
+            return {error: 'payload no existe'}
+        }
+        if (payload.controlNumber == null || typeof payload.controlNumber != 'string'){
+            return {error: 'necesitan tener un valor valido'}
+        }
+        else if(payload.mail == null || typeof payload.mail != 'string'){
+        return {error: 'necesitan tener un valor valido'}
+        }
+        else if(payload.name == null || typeof payload.name != 'string'){
+            return {error: 'necesitan tener un valor valido'}
+        }
+        else if(payload.lastName == null || typeof payload.lastName != 'string'){
+            return {error: 'necesitan tener un valor valido'}
+        }
+        else if(payload.balance == null || typeof payload.balance == isNaN){
+            return {error: 'necesitan tener un valor valido'}
+        }
+        else {
+            return new User(payload.controlNumber, payload.mail, payload.name, payload.lastName, payload.balance)
+             
+        } 
+    }
 }
+
+module.exports = UserView
--- api/test/views/UserView.test.js
+++ api/test/views/UserView.test.js
@@ -8,7 +8,31 @@ describe('Tests for UserView', () => {
         expect(result.error).toMatch(/payload no existe/)
     })
 
-    // test('Return an error object when try to create a new user with a payload with missin properties', ()=>{
-    //     const payload = {numberControl: null, }
-    // })
-})
+    test('Return an error object when try to create a new user with a payload with missin properties', ()=>{
+        const payload = {controlNumber: null, mail: 15, name:null, lastName: 12, balance:'Swag'}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test('Return an error object when try to create a new user with a payload with missing properties', () =>{
+        const payload = {controlNumber: 'Numero de control'}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
+
+    test('Create a user by a given valid payload', () =>{
+        const payload = {
+        controlNumber: '17260671',
+        mail: 'jugo@mail.com',
+        name:'Julian',
+        lastName: 'Gonzalez',
+        balance:2.4}
+        
+        const result = UserView.createUser(payload)
+        expect(result.controlNumber).toBe('17260671')
+        expect(result.mail).toBe('jugo@mail.com')
+        expect(result.name).toBe('Julian')
+        expect(result.lastName).toBe('Gonzalez')
+        expect(result.balance).toBe(2.4)
+    })
+})          
