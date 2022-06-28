fec606e408684c421b5d155ea5935fe25abe7986
Yuizz <julianglz@outlook.es>
2022-06-27 20:51:24 -0500

    Add docker full support for dev environment

--- api/db.js
+++ api/db.js
@@ -1,7 +1,12 @@
-const {MongoClient} = require('mongodb')
-const mongoUrl = "mongodb://jugo:pass@localhost:27017"
-const client = new MongoClient(mongoUrl)
-client.connect()
-const db = client.db('test')
+const { MongoClient } = require("mongodb");
 
-module.exports = db
+const dbUser = process.env.MONGO_INITDB_ROOT_USERNAME;
+const dbPass = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const dbHost = process.env.MONGO_DB_HOST;
+
+const mongoUrl = `mongodb://${dbUser}:${dbPass}@${dbHost}:27017`;
+const client = new MongoClient(mongoUrl);
+client.connect();
+const db = client.db("pet_machine");
+
+module.exports = db;
--- mongo-init/init.js
+++ mongo-init/init.js
@@ -1,41 +1,39 @@
 conn = new Mongo();
 db = conn.getDB("pet_machine");
 
-db.createUser(
-        {
-            user: "jugo",
-            pwd: "pass",
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "mongodb"
-                }
-            ]
-        }
-);
+db.createUser({
+  user: "jugo",
+  pwd: "pass",
+  roles: [
+    {
+      role: "readWrite",
+      db: "mongodb",
+    },
+  ],
+});
 
-db.createCollection('students'); 
+db.createCollection("users");
 
 student1 = {
-  controlNumber: '17260001',
-  name: 'Student1',
-  lastName: 'test',
+  controlNumber: "17260001",
+  name: "Student1",
+  lastName: "test",
   balance: 0,
   createdAt: new Date(),
   updatedAt: new Date(),
 };
 student2 = {
-  controlNumber: '17260002',
-  name: 'Student2',
-  lastName: 'test',
+  controlNumber: "17260002",
+  name: "Student2",
+  lastName: "test",
   balance: 0,
   createdAt: new Date(),
   updatedAt: new Date(),
 };
 student3 = {
-  controlNumber: '17260003',
-  name: 'Student3',
-  lastName: 'test',
+  controlNumber: "17260003",
+  name: "Student3",
+  lastName: "test",
   balance: 0,
   createdAt: new Date(),
   updatedAt: new Date(),
@@ -44,4 +42,3 @@ student3 = {
 db.students.insert(student1);
 db.students.insert(student2);
 db.students.insert(student3);
-
