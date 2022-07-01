6f58130d0b1d927d25ef75a24a811713388cb335
cristian <Cristian_caht@hotmail.com>
2022-07-01 16:30:21 -0500

    Add database name to env variable

--- api/db.js
+++ api/db.js
@@ -2,5 +2,5 @@ const {MongoClient} = require('mongodb')
 const mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DOMAIN}:27017`
 const client = new MongoClient(mongoUrl)
 client.connect()
-const db = client.db('test')
+const db = client.db(process.env.MONGO_DBNAME)
 module.exports = db
