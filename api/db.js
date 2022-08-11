bdc41b075efaae5c87cc770807b833ed54ea5ec0
Yuizz <julianglz@outlook.es>
2022-08-10 21:08:09 -0500

    Setup production

--- api/db.js
+++ api/db.js
@@ -1,6 +1,9 @@
-const {MongoClient} = require('mongodb')
-const mongoUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DOMAIN}:27017`
-const client = new MongoClient(mongoUrl)
-client.connect()
-const db = client.db(process.env.MONGO_DBNAME)
-module.exports = db
+const { MongoClient } = require("mongodb");
+const mongoUrl =
+  process.env.NODE_ENV === "development"
+    ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DOMAIN}:27017`
+    : `mongodb://${process.env.MONGO_DOMAIN}/pet_machine`;
+const client = new MongoClient(mongoUrl);
+client.connect();
+const db = client.db(process.env.MONGO_DBNAME);
+module.exports = db;
